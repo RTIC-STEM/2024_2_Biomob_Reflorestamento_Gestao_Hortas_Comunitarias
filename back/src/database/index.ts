c0b181dde89338237200092eba7e89e9888d92e8
pedro <jpedrosl42@gmail.com>
2025-06-12 18:53:34 -0300

    vercel update

--- back/src/database/index.ts
+++ back/src/database/index.ts
@@ -3,11 +3,11 @@ require('dotenv').config()
 
 export const sequelize = new Sequelize({
     dialect: 'postgres',
-    host: 'dpg-d0sfu63uibrs73ad1o10-a',
-    port: parseInt('5432'),
-    database: 'biomob_banco_de_dados',
-    username: 'biomob_banco_de_dados_user',
-    password: 'c5s2NCMhMcJignbiQjMmbzDRrNzuWX2P',
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.GB_PORT || '5432'),
+    database: process.env.DB_DATABASE,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     define: {
       underscored: true
     }
